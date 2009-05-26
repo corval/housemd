0ca86a5232fb51d985130302317fe6d09f31ff00
sergeant.pepper.01@gmail.com <sergeant.pepper.01@gmail.com@da855334-1dac-11de-b650-e715bd6d7cf1>
2009-05-26 15:52:29 +0000

    Yuhu, hace JUJU -"Dr. House" Proyecto Completo

--- Servicio Medico/src/Principal/PPrincipal.java
+++ Servicio Medico/src/Principal/PPrincipal.java
@@ -38,7 +38,7 @@ public class PPrincipal extends javax.swing.JFrame {
         initComponents();
         this.setLocationRelativeTo(null);
         actualizaTabla();
-        recetaActual = new Receta(0.0f, 0.0f, null, null, null, null, 0);
+        recetaActual = new Receta(0.0f, 0.0f, 0.0f, null, null, null, null, null, null, null);
         this.setIconImage(new ImageIcon(getClass().getResource("/Imagenes/housi.png")).getImage());
     }
 
