c939ce7affca5cb55556604aa6cf4c9308aad7c3
sergeant.pepper.01@gmail.com <sergeant.pepper.01@gmail.com@da855334-1dac-11de-b650-e715bd6d7cf1>
2009-05-26 16:36:37 +0000

    Yuhu, hace JUJU -"Dr. House" BD

--- trunk/Servicio Medico/src/inventario/Producto.java
+++ trunk/Servicio Medico/src/inventario/Producto.java
@@ -1,15 +1,4 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package inventario;
-
-/**
- *
- * @author student6im7
- */
-
 
 public class Producto implements java.io.Serializable{
     private String numCodigoBarras;
@@ -27,44 +16,26 @@ public class Producto implements java.io.Serializable{
         this.cantidad+=cantidad;
     }
 
-    /**
-     * @return the numCodigoBarras
-     */
     public String getNumCodigoBarras() {
         return numCodigoBarras;
     }
 
-    /**
-     * @param numCodigoBarras the numCodigoBarras to set
-     */
     public void setNumCodigoBarras(String numCodigoBarras) {
         this.numCodigoBarras = numCodigoBarras;
     }
 
-    /**
-     * @return the nombre
-     */
     public String getNombre() {
         return nombre;
     }
 
-    /**
-     * @param nombre the nombre to set
-     */
     public void setNombre(String nombre) {
         this.nombre = nombre;
     }
 
-    /**
-     * @return the cantidad
-     */
     public int getCantidad() {
         return cantidad;
     }
 
-    /**
-     * @param cantidad the cantidad to set
-     */
     public void setCantidad(int cantidad) {
         this.cantidad = cantidad;
     }
