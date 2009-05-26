672c0f464557993f84b1af3a9a2a2bcc3750dd26
sergeant.pepper.01@gmail.com <sergeant.pepper.01@gmail.com@da855334-1dac-11de-b650-e715bd6d7cf1>
2009-05-26 16:35:54 +0000

    Yuhu, hace JUJU -"Dr. House" BD

--- trunk/Servicio Medico/src/Alumno/Habitos.java
+++ trunk/Servicio Medico/src/Alumno/Habitos.java
@@ -1,14 +1,5 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package Alumno;
 
-/**
- *
- * @author alumno
- */
 public class Habitos {
     String RFC;
     String OtrasActividades;
--- trunk/Servicio Medico/src/Alumno/Historial.java
+++ trunk/Servicio Medico/src/Alumno/Historial.java
@@ -1,15 +1,10 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package Alumno;
 
-/**
- *
- * @author alumno
- */
+import java.sql.*;
+import javax.swing.JOptionPane;
+
 public class Historial {    
+    AgudezaVisual agVis;
     String Nombre;
     String Apellido_Paterno;
     String Apellido_Materno;
@@ -17,4 +12,41 @@ public class Historial {
     String Boleta;
     String Sexo;
     int Edad;
+
+    //Varibles, para la Base de Datos
+    Connection conexion;
+    Statement stmt;
+    ResultSet rs;
+    String login = "root";
+    String password = "pass";
+    String url = "jdbc:mysql://192.168.0.48:3306/test";
+
+    public void sacaAlumno(String aBuscar) {
+        try {
+            Class.forName("com.mysql.jdbc.Driver").newInstance();
+            conexion = DriverManager.getConnection(url, login, password);
+            if (conexion != null) {
+                System.out.println("Conexión a base de datos " + url + " ... Ok");
+                stmt = conexion.createStatement();
+                rs = stmt.executeQuery ("select * from historial WHERE (RFC LIKE '"+ aBuscar +"')");
+                if(rs.next()){
+                    Nombre = rs.getString("Nombre");
+                    Apellido_Paterno = rs.getString("Apellido_Paterno");
+                    Apellido_Materno = rs.getString("Apellido_Materno");
+                    Boleta = rs.getString("Boleta");
+                    Sexo = rs.getString("Sexo");
+                    Edad = Integer.parseInt(rs.getString("Edad"));
+                    rs = stmt.executeQuery ("select * from agudezavisual WHERE (RFC LIKE '"+ aBuscar +"')");
+                    agVis.Derecho = Integer.parseInt(rs.getString("Derecho"));
+                    agVis.Izquierdo = Integer.parseInt(rs.getString("Izquierdo"));
+                }else{
+                    JOptionPane.showMessageDialog(null ,"Lo sentimos no existe ese RFC", "Error", JOptionPane.ERROR_MESSAGE);
+                    conexion.close();
+                }
+            }
+        }catch (Exception e) {
+            System.out.println("Hubo un problema al intentar conectarse con la base de datos " + url);
+        }
+    }
+
 }
